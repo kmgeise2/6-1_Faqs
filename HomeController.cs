aecb692a005e7879971d1050494d5c6c250d847d
Kathleen Geise <92229098+kmgeise2@users.noreply.github.com>
2022-02-12 21:43:57 -0700

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Faqs.Models;
+
+namespace Faqs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private FaqsContext context { get; set; }
+
+        public HomeController(FaqsContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index(string topic, string category)
+        {
+            ViewBag.Topics = context.Topics.OrderBy(t => t.Name).ToList();
+            ViewBag.Categories = context.Categories.OrderBy(c => c.Name).ToList();
+            ViewBag.SelectedTopic = topic;
+
+            IQueryable<FAQ> faqs = context.FAQs
+                .Include(f => f.Topic)
+                .Include(f => f.Category)
+                .OrderBy(f => f.Question);
+
+            if (!string.IsNullOrEmpty(topic)) {
+                faqs = faqs.Where(f => f.TopicId == topic);
+            }
+            if (!string.IsNullOrEmpty(category)) {
+                faqs = faqs.Where(f => f.CategoryId == category);
+            }
+
+            return View(faqs.ToList());
+        }
+    }
+}
